282a72f0709963c0dfb998775feeb9270aa9f4f5
Nikita <rus1703157@gmail.com>
2020-03-22 23:08:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+    class Token {
+        constructor(value,key) {
+            this.value = value;
+            this.key = key;
+        }
+    }
+    
+    let keyArr = [];
+    let temp;
+    for(let i =0; i<bracketsConfig.length; i++){
+        for(let j =0; j<bracketsConfig[i].length; j++){
+          if(j==0){
+            temp = new Token(bracketsConfig[i][j], true);
+          } else if(j==1&&bracketsConfig[i][j]=="|"){
+            temp = new Token(bracketsConfig[i][j], true);  
+          } else {
+            temp = new Token(bracketsConfig[i][j], false);
+          }
+       keyArr.push(temp);  
+        
+    }
+    }
+    
+    
+    let arr = str.split("");
+    if(arr.length%2!=0){
+        return false;
+    }
+    let x;
+    let y;
+    for(let i=0;i<arr.length;i++){
+      for(let j=0;j<keyArr.length;j++){
+        if(arr[i]==keyArr[j].value){
+          x = keyArr[j].value;
+          y = keyArr[j].key;
+          for(let k=0;k<arr.length;k++){
+            if(arr[k]==x && arr[k]!=y){
+              arr.splice(k,1);
+              arr.splice(i,1);
+              i--;
+            }
+          }
+        }
+    }
+    }
+    
+    
+    if(arr.length==0){
+      return true;
+    } else return false;
+    
+    }
